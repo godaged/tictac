abe79f3dd3d1b959f833ea97e4cb1e2b11d746f0
Dhana Godage <godaged@yahoo.com>
2021-02-23 19:51:25 -0500

    Refactored existing code

--- tictactoe.py
+++ tictactoe.py
@@ -5,8 +5,9 @@ board = ["", "", "", "", "", "", "", "", "", ]
 pc = ""
 player = ""
 leftPadding = " " * 5
+moves = range(1,10)
 
-def isBoadEmpty(board, index):
+def isBoardEmpty(board, index):
     # return " " if cell is empty else return cell value.
     cellValue = ""
     if len(board[index]) > 0:
@@ -14,29 +15,6 @@ def isBoadEmpty(board, index):
     else:
         cellValue = " "
     return cellValue
-
-def printBoard():
-    # prints the tic tac toe board.
-    #os.system('cls ||clear')
-    row = ''
-    print(leftPadding, "Tic Tac Toe")
-    print(leftPadding, "===========")
-
-    for index in range(len(board)):
-        # start index with 1 => 0 + 1.
-        index += 1
-        if index%3 == 0:
-            # print row if reached to thirs cell of the row.
-            row += str(isBoadEmpty(board, index - 1))
-            devider = '\n {}---|----|---'.format(leftPadding)
-            if index == 9:
-                # do not print devider if reached to 9th cell
-                devider = ''
-            print(leftPadding, row, devider)
-            row = ''
-        if index%3 != 0:
-            # set first and second sell value content of the row.
-            row += str(isBoadEmpty(board, index-1)) + '  | '
 
 def selectMarker():
     # set the marker of your choice
@@ -48,32 +26,69 @@ def selectMarker():
             marker = ""  
     return marker
 
-def makeMove(board, player):
+def printBoard(board):
+    # prints the tic tac toe board.
+    os.system('cls')
+    row = ''
+    print(leftPadding, "Tic Tac Toe")
+    print(leftPadding, "===========")
+
+    for index in moves:
+        # start index with 1.
+        if index%3 == 0:
+            # print row and devider if reached to third cell of the row.
+            row += str(isBoardEmpty(board, index - 1))
+            devider = '\n {}---|----|---'.format(leftPadding)
+            if index == 9:
+                # do not print devider if reached to 9th cell
+                devider = ''
+            print(leftPadding, row, devider)
+            row = ''
+        #if index%3 != 0:
+        else:
+            # set first and second sell value content of the row.
+            row += str(isBoardEmpty(board, index-1)) + '  | '
+        if board.count("") == 0:
+            exit()
+
+def getLocation(player):
+    # get only integer
+    question = " '{}', What place do you want to mark (1-9)? ".format(player)
+    move = input(question)
+    while not move.isnumeric():
+        print(" '{}' is not a valid entry".format(move.upper()) + "\n  Enter number between 1 and 9")
+        move = input(question)
+    return int(move)
+
+def playerMove(board, player):
     # make your move 1 to 9 otherwise out of range
     # if already filled, select empty cell
     move = 1
-    moves = range(1,10)
     while move in moves:
-        move = int(input(" Select what place you want to mark?"))
+        move = getLocation(player)
         if move not in moves:
             print(" {} is out of range".format(move))
             move = 1
-        elif isBoadEmpty(board, move-1) == " ":
+        elif isBoardEmpty(board, move-1) == " ":
             board[move-1] = player
             break
         else:
-            print(" {} th place is already taken".format(move))
+            print(" {} th place is already taken.".format(move))
 
-def playGame():
+def playGame(board):
     # plays the game.
     player = selectMarker()
     pc = 'O' if player == 'X' else 'X'
-    printBoard()
-    print()
-    print(leftPadding, " Player = {}\n".format(player), leftPadding, "PC = {}".format(pc))
-    makeMove(board, player)
-    printBoard()
-    pass
+    message = "\n" + leftPadding + " Player = " + player + '\n' + leftPadding + " PC = " + pc
+    printBoard(board)
+    for _ in range(5):
+        print(message)
+        playerMove(board, player)
+        printBoard(board)
+        print(message)
+        playerMove(board, pc)
+        printBoard(board)
+        pass
 
 if __name__ == "__main__":
-    playGame()
+    playGame(board)
